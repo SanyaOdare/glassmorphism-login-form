51423e832cc41d1c17310d53ed70c4c27bcb362b
SanyaOdare <sanyaodare@gmail.com>
2023-04-06 12:55:46 +0100

    add interactivity

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-console.log("Ready or not")
+const showHiddenPassword = (loginPassword, loginEye) => {
+  const input = document.getElementById(loginPassword),
+      iconEye = document.getElementById(loginEye)
+  
+      iconEye.addEventListener('click', () => {
+        // change password to text
+        if (input.type === 'password') {
+          // switch to text
+          input.type = 'text'
+
+          // change icon
+          iconEye.classList.add('ri-eye-line')
+          iconEye.classList.remove('ri-eye-off-line')
+        } else {
+          input.type = 'password'
+
+          // change icon
+          iconEye.classList.remove('ri-eye-line')
+          iconEye.classList.add('ri-eye-off-line')
+        }
+      })
+}
+
+showHiddenPassword('login-password', 'login-eye')
